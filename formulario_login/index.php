c40ca0f3488eb83c1d18925d9ec72169305032bf
Gaston-Escudero <gastonescudero95@gmail.com>
2022-07-23 16:59:35 -0300

    cambios

--- formulario_login/index.php
+++ formulario_login/index.php
@@ -4,14 +4,15 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 
-IF($_POST){
-    $usuario = $_POST["txtUsuario"];
-    $clave = $_POST["txtClave"];
+if($_POST){ //es postback?
+    $usuario = $_REQUEST["txtUsuario"];
+    $clave = $_REQUEST["txtClave"];
 
-    if($usuario == "admin" && $clave == "123456"){
-        header("Location: acceso-corfirmado.php");
+    //Si usuario es distinto de vacio y clave es distinto de vacio, entonces:
+    if($usuario != "" && $clave != ""){
+        header("Location: acceso-confirmado.php");
     } else {
-        $msg ="Usuario o clave incorrecto";
+        $mensaje ="Usuario o clave incorrecto";
     }
 }
 
@@ -27,18 +28,23 @@ IF($_POST){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
     <main class="container">
         <div class="row">
-            <div class="col-12 text-center">
+            <div class="col-12 text-center py-5">
                 <h1>Formulario</h1>
             </div>
         </div>
         <div class="row">
             <div class="col-6">
-                <form action="" method="post">
+                <?php if( isset($mensaje)):?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $mensaje; ?>
+                    </div>    
+                <?php endif; ?>    
+                <form method="POST" action="">
                     <div class="py-3">
                         <label for="txtUsuario">Usuario:</label>
                         <input class="form-control" type="text" name="txtUsuario" id="txtUsuario">
@@ -49,6 +55,7 @@ IF($_POST){
                     </div>
                     <div class="py-3">
                         <button class="btn btn-primary" type="submit">Ingresar</button>
+                        
                     </div>
 
                 </form>
