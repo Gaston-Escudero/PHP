ee18772c0918fae0c137c8a6a7dc551e45e8d7d7
Gaston-Escudero <gastonescudero95@gmail.com>
2022-07-27 04:53:46 -0300

    cambios

--- clientes_session.php
+++ clientes_session.php
@@ -12,7 +12,8 @@ if(isset($_SESSION["listadoClientes"])){
     $aClientes = array();
 }
 
-if($_POST){
+// si hace click en enviar entonces:
+if(isset($_POST["btnEnviar"])){
     //Asignamos en variables los datos que vienen del formulario
     $nombre = $_POST["txtNombre"];
     $dni = $_POST["txtDni"];
@@ -29,7 +30,21 @@ if($_POST){
 $_SESSION["listadoClientes"] = $aClientes;
 
 }
-    
+//si hace click en eliminar entonces:
+if(isset($_POST["btnEliminar"])){
+    session_destroy();
+    $aClientes = array();
+}
+
+if(isset($_GET["pos"])){
+    //recupero el dato que viene desde la query string via get
+    $pos = $_GET["pos"];
+    //elimina la posicion del array indicada
+    unset($aClientes[$pos]);
+    //actualizo la variable de session con el array actualizado
+    $_SESSION["listadoClientes"] = $aClientes;
+    header("Location: clientes_session.php");
+}
 
 
 ?>
@@ -42,6 +57,7 @@ $_SESSION["listadoClientes"] = $aClientes;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de Clientes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 <body>
     <main class="container">
@@ -70,8 +86,8 @@ $_SESSION["listadoClientes"] = $aClientes;
                         <input type="number" id="txtEdad" name="txtEdad" class="form-control">
                     </div>
                     <div class="mb-3">
-                        <button type="submit" class="btn btn-primary d-inline">Enviar</button>
-                        <button type="submit" class="btn btn-danger d-inline">Eliminar</button>
+                        <button type="submit"  name="btnEnviar" class="btn btn-primary d-inline">Enviar</button>
+                        <button type="submit" name="btnEliminar" class="btn btn-danger d-inline">Eliminar</button>
                     </div>
                 </form>
             </div>
@@ -83,15 +99,17 @@ $_SESSION["listadoClientes"] = $aClientes;
                             <th>DNI:</th>
                             <th>Teléfono:</th>
                             <th>Edad:</th>
+                            <th>Acciones:</th>
                         </tr>
                    </thead>
                    <tbody>
-                        <?php foreach ($aClientes as $cliente): ?>
+                        <?php foreach ($aClientes as $pos => $cliente): ?>
                             <tr>
                                 <td><?php echo $cliente["nombre"]?></td>
                                 <td><?php echo $cliente["dni"]?></td>
                                 <td><?php echo $cliente["telefono"]?></td>
                                 <td><?php echo $cliente["edad"]?></td>
+                                <td><a href="clientes_session.php?pos=<?php echo $pos; ?>"><i class="bi bi-trash"></i></a></td>
                             </tr>
                         <?php endforeach; ?>
                    </tbody>
