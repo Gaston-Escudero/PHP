e212d37ae476bad7644367967fe5db583129f87a
Gaston-Escudero <gastonescudero95@gmail.com>
2022-07-27 06:32:33 -0300

    cambios

---
+++ print_f.php
@@ -0,0 +1,28 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//definicion
+function print_f($variable){
+    if(is_array($variable)){
+        $archivo = fopen ('datos.txt','a+');
+        //si es un array, lo recorro y guardo el contenido en el archivo "datos.txt"
+        foreach ($variable as $item){
+            fwrite($archivo, $item);
+        }
+        fclose($archivo);
+
+    } else {
+        //entonces es string, guardo el contenido en el archivo "datos.txt"
+        file_put_contents("datos.txt",$variable);
+    }
+    echo "Archivo generado.";
+}
+
+//uso
+$aNotas = array(8,5,7,9,10);
+$msg = "Este es un mensaje!";
+print_f($msg);
+?>
